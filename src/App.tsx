6664370e94aa5ddea484c578fc66ebb701109591
Salvatore Fara <sf.salvatorefara@gmail.com>
2024-12-18 22:33:33 +0000

    Idle and New Note states

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import createTuner from "@pedroloch/tuner";
 import { TunerData } from "@pedroloch/tuner/dist/interfaces";
 import useMicrophoneVolume from "react-use-microphone-volume-hook";
 import { C0, Notes } from "./constants";
+import { Note, PracticeState } from "./types";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -16,19 +17,29 @@ const defaultTunerData: TunerData = {
 
 const volumeThreshold = 30;
 
-const tuner = createTuner();
+const pitchTracker = createTuner();
 
 function getOctave(pitch: number) {
   return Math.floor(Math.log2(pitch / C0));
 }
 
 function App() {
+  const [practiceState, setPracticeState] = useState<PracticeState>("Idle");
+  const [currentNote, setCurrentNote] = useState<Note>(Notes[0]);
   const [isListening, setIsListening] = useState(false);
   const [tunerData, setTunerData] = useState<TunerData>(defaultTunerData);
   const [
     volume,
     { startTrackingMicrophoneVolume, stopTrackingMicrophoneVolume },
   ] = useMicrophoneVolume();
+
+  const handlePractice = () => {
+    if (practiceState == "Idle") {
+      setPracticeState("New Note");
+    } else {
+      setPracticeState("Idle");
+    }
+  };
 
   const handleListening = () => {
     if (isListening) {
@@ -41,26 +52,41 @@ function App() {
   const enableListening = () => {
     const status = startTrackingMicrophoneVolume();
     if (status) setIsListening(true);
-    tuner.start();
+    pitchTracker.start();
   };
 
   const disableListening = () => {
     const status = stopTrackingMicrophoneVolume();
     if (status) setIsListening(false);
-    tuner.stop();
+    pitchTracker.stop();
   };
+
+  useEffect(() => {
+    console.log("Practice state:", practiceState);
+
+    switch (practiceState) {
+      case "Idle":
+        break;
+      case "New Note":
+        const randomNote = Notes[Math.floor(Math.random() * Notes.length)];
+        setCurrentNote(randomNote);
+        setPracticeState("Listening");
+        enableListening();
+        break;
+      case "Listening":
+        break;
+    }
+  }, [practiceState]);
 
   useEffect(() => {
     console.log("Listening Changed", isListening);
   }, [isListening]);
 
-  tuner.getData((data) => {
+  pitchTracker.getData((data) => {
     if (volume >= volumeThreshold) {
       setTunerData(data);
     }
   });
-
-  const random_note = Notes[Math.floor(Math.random() * Notes.length)];
 
   return (
     <>
@@ -74,8 +100,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={handleListening}>
-          {isListening ? "Stop practice" : "Start practice"}
+        <button onClick={handlePractice}>
+          {practiceState == "Idle" ? "Start practice" : "Stop practice"}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
@@ -84,7 +110,7 @@ function App() {
       <p className="read-the-docs">
         Volume {volume}, Tuner {tunerData.note} {tunerData.pitch}, Octave
         {getOctave(tunerData.pitch)}, Random Note
-        {random_note.name} {random_note.octave}
+        {currentNote.name} {currentNote.octave}
       </p>
     </>
   );
--- src/types/index.tsx
+++ src/types/index.tsx
@@ -2,3 +2,10 @@ export type Note = {
   name: string;
   octave: number;
 };
+
+export type PracticeState =
+  | "Idle"
+  | "New Note"
+  | "Listening"
+  | "Feedback"
+  | "Pause";
