c32b90e86bff085550663086b8e3c37c24bb4081
Salvatore Fara <sf.salvatorefara@gmail.com>
2025-01-07 08:51:38 +0000

    increased buffer size more

--- public/audio/BufferProcessor.js
+++ public/audio/BufferProcessor.js
@@ -3,7 +3,7 @@
 */
 class BufferProcessor extends AudioWorkletProcessor {
   // 0. Determine the buffer size (this is the same as the 1st argument of ScriptProcessor)
-  bufferSize = 4096;
+  bufferSize = 8192;
   // 1. Track the current buffer fill level
   _bytesWritten = 0;
 
